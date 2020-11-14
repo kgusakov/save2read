df3608907562a9683ddf65074758623bae0fd695
Kirill Gusakov <kgusakov@gmail.com>
2020-11-14 22:43:28 +0300

    Added templating

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,43 @@
+mod storage;
+
 use actix_web::{get, post, web, App, HttpResponse, HttpRequest, HttpServer, Responder};
 use url::Url;
 use std::sync::Mutex;
-
-
-mod storage;
-
-use  storage::Storage;
+use askama::Template;
+use storage::Storage;
 
 struct AppState {
     storage: Mutex<Storage>
+}
+
+#[derive(Template)] // this will generate the code...
+#[template(path = "index.html")] // using the template in this path, relative
+                                 // to the `templates` dir in the crate root
+struct ListTemplate<'a> { // the name of the struct can be anything
+    app_name: &'a str,
+    links: &'a Vec<String>
 }
 
 
 #[get("/list/{user_id}")]
 async fn list(web::Path(user_id): web::Path<String>, data: web::Data<AppState>) -> impl Responder {
     let d = data.storage.lock().unwrap();
-    let body = d.pending_list(user_id)
+    let links = d.pending_list(user_id)
         .await
         .iter()
         .map(|url| url.to_string())
-        .collect::<Vec<String>>()
-        .join("\n");
+        .collect::<Vec<String>>();
 
-    HttpResponse::Ok().body(body)
+    HttpResponse::Ok().body(
+        ListTemplate {
+            app_name: "Save For Read",
+            links: &links
+        }.render().unwrap())
 }
 
 #[post("/add/{user_id}")]
 async fn add(request: web::Bytes, web::Path(user_id): web::Path<String>,  data: web::Data<AppState>)  -> impl Responder {
     let mut d = data.storage.lock().unwrap();
-    println!("url: {}, user_id: {}", String::from_utf8(request.to_vec()).unwrap(), user_id);
     d.add(&user_id, Url::parse(&String::from_utf8(request.to_vec()).unwrap()).unwrap()).await;
     HttpResponse::Ok().body("Added!")
 }
