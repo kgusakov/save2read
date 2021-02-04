6243bb5647d8784ef13b60d1adc4a0af37ae4244
Kirill Gusakov <kgusakov@gmail.com>
2021-02-05 00:12:48 +0300

    Fixed title tag + support of redirects

--- src/extractor.rs
+++ src/extractor.rs
@@ -1,11 +1,15 @@
 use std::str::from_utf8;
 
-use actix_web::client::Client;
+use actix_web::http;
+use actix_web::{
+    client::Client,
+    web::Bytes,
+};
 use anyhow::{anyhow, Context, Result};
 use scraper::{Html, Selector};
 
 fn title(doc: &Html) -> Result<Option<String>> {
-    let title_selector = Selector::parse("head > title")
+    let title_selector = Selector::parse("title")
         .map_err(|err| anyhow!("Can't parse selector for titile {:?}", err))?;
     match doc.select(&title_selector).next() {
         Some(el) => {
@@ -23,23 +27,51 @@ fn title(doc: &Html) -> Result<Option<String>> {
 
 pub async fn extract(url: &url::Url) -> Result<Option<String>> {
     let client = Client::default();
-    let resp: Vec<u8> = client
-        .get(url.as_str())
-        .send()
-        .await
-        .map_err(|err| {
-            anyhow!(
-                "Can't send request for data extraction to url {} with error {:?}",
-                url,
-                err
-            )
-        })?
-        .body()
-        .limit(usize::MAX)
-        .await?
-        .to_vec();
-    let html_str =
-        from_utf8(&resp).with_context(|| "Can't convert byte response to string".to_string())?;
-    let html = Html::parse_document(html_str);
-    Ok((title(&html))?)
+    if let Some(data) = ignore_redirects(&client, url.as_str(), 10).await? {
+        let resp: Vec<u8> = data.to_vec();
+        let html_str = from_utf8(&resp)
+            .with_context(|| "Can't convert byte response to string".to_string())?;
+        let html = Html::parse_document(html_str);
+        Ok((title(&html))?)
+    } else {
+        Ok(None)
+    }
+}
+
+async fn ignore_redirects(client: &Client, url: &str, max_redirect: i8) -> Result<Option<Bytes>> {
+    let mut resp = client.get(url).send().await.map_err(|err| {
+        anyhow!(
+            "Can't send request for data extraction to url {} with error {:?}",
+            url,
+            err
+        )
+    })?;
+    let mut redirects = max_redirect;
+    while resp.status().is_redirection() && redirects > 0 {
+        let location = resp.headers().get_all(http::header::LOCATION).last();
+        if let Some(loc) = location {
+            let str_loc = loc.to_str().map_err(|err| {
+                anyhow!(
+                    "Url {} can be parsed to string for receiving title, error: {:?}",
+                    url,
+                    err
+                )
+            })?;
+            resp = client.get(str_loc).send().await.map_err(|err| {
+                anyhow!(
+                    "Can't send request for data extraction to url {} with error {:?}",
+                    url,
+                    err
+                )
+            })?;
+        } else {
+            break;
+        }
+        redirects = redirects - 1;
+    }
+    if !resp.status().is_redirection() {
+        Ok(Some(resp.body().limit(usize::MAX).await?))
+    } else {
+        Ok(None)
+    }
 }
